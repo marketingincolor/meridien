90a7361b5a7e0801febdd9eb7bfecf4722eaf1cc
nbeers22 <nbeers22@gmail.com>
2019-01-02 17:40:38 -0500

    Created AJAX filter of results on studies archive page

--- archive-studies.php
+++ archive-studies.php
@@ -16,10 +16,56 @@ get_header(); ?>
 					</div>
 					<div class="cell small-12 text-center">
 
+						<?php //echo do_shortcode('[searchandfilter hide_empty="0" fields="indications,study_location" types="multiselect,multiselect" headings="Indication,Location" submit_label="Filter Studies" post_types="studies" class="studies-list"]'); ?>
+						<!-- <form action="<?php echo site_url('/studies/'); ?>" method="post" class="searchandfilter reset"><input type="submit" value="Clear"></form> -->
 
-						<?php echo do_shortcode('[searchandfilter hide_empty="0" fields="indications,study_location" types="select,multiselect" headings="Indication,Location" submit_label="Filter" post_types="studies" class="studies-list"]'); ?>
-						<form action="<?php echo site_url('/studies/'); ?>" method="post" class="searchandfilter reset"><input type="submit" value="Clear"></form>
-					
+						<form>
+						  <div class="grid-x grid-margin-x grid-margin-y">
+								<div class="medium-6 cell">
+									<label>Indications
+										<select class="" name="indications" id="indications-select" multiple>
+											<option value="all" selected>All Indications</option>
+
+											<?php
+												$terms = get_terms( array(
+												    'taxonomy' => 'indications',
+												    'hide_empty' => false,
+												));
+												foreach ($terms as $term) {
+											?>
+																
+											<option value="<?php echo $term->slug; ?>"><?php echo $term->name; ?></option>
+																
+										  <?php } ?>
+										</select>
+									</label>
+								</div>
+							
+								<div class="medium-6 cell">
+									<label>Locations
+										<select class="" name="locations" id="locations-select" multiple>
+											<option value="all" selected>All Locations</option>
+											
+												<?php
+													$terms = get_terms( array(
+													    'taxonomy' => 'study_location',
+													    'hide_empty' => false,
+													));
+													foreach ($terms as $term) {
+												?>
+																	
+												<option value="<?php echo $term->slug; ?>"><?php echo $term->name; ?></option>
+																	
+											  <?php } ?>
+
+										</select>
+									</label>
+								</div>
+								<div class="small-12 cell">
+									<button id="filter-studies" class="orange-button" type="submit">Filter Studies</button>
+								</div>
+						  </div>
+						</form>
 
 					</div>
 					<div class="cell small-12 text-center">
@@ -28,7 +74,7 @@ get_header(); ?>
 				</div>
 
 				<!-- <div class="grid-x grid-padding-x grid-margin-x small-up-2 medium-up-4"> -->
-				<div class="grid-x grid-margin-x page-grid" data-equalizer> 
+				<div id="results-container" class="grid-x grid-margin-x page-grid" data-equalizer> 
 
 		    	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<!-- To see additional archive styles, visit the /parts directory -->
@@ -70,7 +116,7 @@ get_header(); ?>
 							</div>
 							<?php //} ?>
 						</section> <!-- end article section -->
-											
+
 						<footer class="article-footer">
 					    	<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'jointstheme') . '</span> ', ', ', ''); ?></p>
 						</footer> <!-- end article footer -->	
