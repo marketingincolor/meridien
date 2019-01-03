6e9f7d7b273c5284c1d83c336b537e249846dd98
Edd Twilbeck <etwilbeck@marketingincolor.com>
2019-01-03 16:28:24 -0600

    minor layout tweaks for serivces

--- parts/loop-archive-services.php
+++ parts/loop-archive-services.php
@@ -12,11 +12,11 @@ if ($post_type == 'services' ) {
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">			
 	
 	<header class="article-header">
-		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
 	</header> <!-- end article header -->
 					
 	<section class="entry-content" itemprop="text">
-		<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
+		<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 		<?php //the_content('<button class="tiny">' . __( 'Read more...', 'jointswp' ) . '</button>'); ?>
 		<?php the_excerpt(); ?>
 	</section> <!-- end article section -->
